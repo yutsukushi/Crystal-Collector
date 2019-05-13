d646477ee5c0edb85aa78fcd65199e4a421ff3da
yutsukushi <ykushibi@gmail.com>
2019-05-13 17:21:19 -0400

    Set up HTML and JavaScript content with proper tags and variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Variables
+
+var chosenNumber = $(".chosenNumber");
+var winCounter = $(".winCounter");
+var lossCounter = $(".lossCounter");
+var yourNumber = $(".yourNumber");
+var crystalValue = $(".crystalValue");
+
+$(document).ready(function() { //Checking if the page is ready to load the game.
+
+    function initializeGame() { //Initializing the game on page load.
+
+    }
+
+    function randCompNumber() { //Generates a random computer number for the user to try and match for every game start
+
+    }
+
+    function userNumber() { //Being able to add randomly assigned crystal values to the user number
+
+    }
+
+    function assignCrystalValue() { //Assigning the crystals different values every time game initiates
+
+    }
+
+    function winCounter() { //Counts user wins if user is able to match their number to the computer number
+
+    }
+
+    function lossCounter() { //Counts user loss if user goes over the computer number.
+
+    }
+
+    initializeGame(); //Starts game when page loads.
+
+});
