3ce2c3fca907b8972a0f8c57f9d1ee0293038378
yutsukushi <ykushibi@gmail.com>
2019-05-14 23:04:44 -0400

    Broke code, updated other functions of the game, still have to figure out how to assign values to different crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,11 @@ var lossCounter = $(".lossCounter");
 var yourNumber = $(".yourNumber");
 
 var crystalValueArr;
+var crystalValue = $(".crystalValue");
+var crystalValueTwo = $(".crystalValueTwo");
+var crystalValueThree = $(".crystalValueThree");
+var crystalValueFour = $(".crystalValueFour");
+
 // var crystalValueOne = crystalValueArr[0];
 // // var crystalValueTwo = crystalValueArr[1];
 // // var crystalValueThree = crystalValueArr[2];
@@ -22,12 +27,25 @@ $(document).ready(function() { //Checking if the page is ready to load the game.
        chosenNumber.text("Match my number: " + [Math.floor(Math.random() * 120) + 19]); //chosen number will be anywhere between 19 - 120.
 
        yourNumber.text(0); //displaying user score on start of the game
+       
+       winCounter();
+       lossCounter();
+       assignCrystalValue();
 
-       function crystalValues() { //generating crystal values
+    }
+
+
+    function userNumber() { //Being able to add randomly assigned crystal values to the user number
+
+        yourNumber += crystalValue;
+
+    }
+
+    function assignCrystalValue() { //Assigning the crystals different values every time game initiates
 
         $(".crystalValue").on("click", function() {
         
-          //Currently this will only output the same number amongst the 4 crystals.
+            //Currently this will only output the same number amongst the 4 crystals.
 
         for (var i = 0; i < 4; i++) { //Loops 4 times
 
@@ -35,56 +53,51 @@ $(document).ready(function() { //Checking if the page is ready to load the game.
 
             var crystalValue = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
             
-            if ((crystalValueArr === -1) || 
-            (crystalValueArr === crystalValueArr[0]) || 
-            (crystalValueArr === crystalValueArr[1]) || 
-            (crystalValueArr === crystalValueArr[2]) || 
-            (crystalValueArr === crystalValueArr[3])) {
+            if ((crystalValueArr === -1) || (crystalValueArr === crystalValueArr[3])) {
 
-            crystalValueArr.push(crystalValue);
+            crystalValueArr[0].push(crystalValue);
+            crystalValueArr[1].push(crystalValueTwo);
+            crystalValueArr[2].push(crystalValueThree);
+            crystalValueArr[3].push(crystalValueFour);
             
-            // crystalValueArr.push(crystalValue);
-            // crystalValueArr.push(crystalValue);
-            // crystalValueArr.push(crystalValue);
-            
-        }
+            // console.log(crystalValueArr);
+            console.log("clicked the crystal");
+            console.log(crystalValue);
+
             //Find a way to assign 4 different values to each crystal
-        
-        
-        
-        console.log("clicked the crystal");
-        console.log(crystalValue);
-       
-       }
+            // crystalValue.attr(crystalValueArr[0]);
+            // crystalValueTwo = crystalValueArr[1];
+            // crystalValueThree = crystalValueArr[2];
+            // crystalValueFour = crystalValueArr[3];
 
-    });
-        
-}
+            }
+        }
 
-       crystalValues();
-
-    function randCompNumber() { //Generates a random computer number for the user to try and match for every game start
-
-    }
-
-    function userNumber() { //Being able to add randomly assigned crystal values to the user number
-
-    }
-
-    function assignCrystalValue() { //Assigning the crystals different values every time game initiates
-
+        });
     }
 
     function winCounter() { //Counts user wins if user is able to match their number to the computer number
+
+        if (yourNumber === chosenNumber) {
+
+            winCounter = 0;
+            winCounter++;
+
+        }
 
     }
 
     function lossCounter() { //Counts user loss if user goes over the computer number.
 
-    }
+        if (yourNumber > chosenNumber) {
+
+            lossCounter = 0;
+            lossCounter++;
+
+        }
 
     }
 
     initializeGame(); //Starts game when page loads.
     
-});
+});    
