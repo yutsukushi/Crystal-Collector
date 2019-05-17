98ac6618a1fdcb27217f7eed80408f1b298b01d1
yutsukushi <ykushibi@gmail.com>
2019-05-17 14:43:23 -0400

    functioning user number score that adds all the crystals clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,112 @@
-// // Variables
+// Variables
+
+var chosenNumber = $(".chosenNumber");
+var winCounter = $(".winCounter");
+var lossCounter = $(".lossCounter");
+// var yourNumber = $(".yourNumber");
+var yourNumber = 0;
+
+var crystalValueArr;
+var crystalValueOne = $("#crystalValue");
+var crystalValueTwo = $("#crystalValueTwo"); //crystalValueArr[1];
+var crystalValueThree = $("#crystalValueThree"); //crystalValueArr[2];
+var crystalValueFour = $("#crystalValueFour"); //crystalValueArr[3];
+
+$(document).ready(function() { //Checking if the page is ready to load the game.
+
+    function initializeGame() { //Initializing the game on page load.
+
+       chosenNumber.text("Match my number: " + [Math.floor(Math.random() * 120) + 19]); //chosen number will be anywhere between 19 - 120.
+       
+       crystalValues();
+       userNumber();
+       winCounter();
+       lossCounter();
+
+    }
+
+    function crystalValues() { //generating crystal values
+        
+        crystalValueArr = []; //empty crystal value array to push 4 values into
+        
+        for (var i = 0; i < 4; i++) { //Loops 4 times
+            
+            var crystalValueStat = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
+
+            var intCrystalValue = parseInt(crystalValueStat); //changes output from crystalValueStat to an integer in an array.
+
+            crystalValueArr.push(intCrystalValue); //pushes the value into the crystalValueArr.
+            
+            console.log("crystal one: " + crystalValueStat);
+        }
+        // var addCrystals = yourNumber += crystalValueArr[i];
+        // yourNumber += crystalValueArr[i];
+
+        assignCrystalValue();
+        console.log(crystalValueArr);
+            
+    }
+
+    function assignCrystalValue() { //Assigning the crystals different values every time game initiates
+
+        //crystal buttons
+        
+        $("#crystalValue").on("click", function() {
+                
+            $(".yourNumber").html(yourNumber += crystalValueArr[0]);
+            
+        }); 
+        
+        $("#crystalValueTwo").on("click", function() {
+            $(".yourNumber").html(yourNumber += crystalValueArr[1]);
+            
+        }); 
+
+        $("#crystalValueThree").on("click", function() {
+
+            $(".yourNumber").html(yourNumber += crystalValueArr[2]);
+        }); 
+
+        $("#crystalValueFour").on("click", function() {
+
+            $(".yourNumber").html(yourNumber += crystalValueArr[3]);
+        }); 
+        
+    }
+
+    function userNumber() { //Being able to add randomly assigned crystal values to the user number
+        
+        // TODO: be able to add two value that are clicked together
+        
+        
+    }
+
+    function winCounter() { //Counts user wins if user is able to match their number to the computer number
+
+        if (yourNumber === chosenNumber) {
+
+            winCounter = 0;
+            winCounter++;
+            
+        }
+    }
+
+    function lossCounter() { //Counts user loss if user goes over the computer number.
+
+        if (yourNumber > chosenNumber) {
+
+            lossCounter = 0;
+            lossCounter++;
+    
+        }
+    }
+
+    initializeGame(); //Starts game when page loads.
+    
+});
+
+// --------------------------------------------------------------------------------------
+// Variables
 
 // var chosenNumber = $(".chosenNumber");
 // var winCounter = $(".winCounter");
@@ -109,197 +217,3 @@
     
 // });    
 
-// Variables
-
-var chosenNumber = $(".chosenNumber");
-var winCounter = $(".winCounter");
-var lossCounter = $(".lossCounter");
-// var yourNumber = $(".yourNumber");
-var yourNumber = 0;
-
-var crystalValueArr;
-var crystalValueOne = $("#crystalValue");
-var crystalValueTwo = $("#crystalValueTwo"); //crystalValueArr[1];
-var crystalValueThree = $("#crystalValueThree"); //crystalValueArr[2];
-var crystalValueFour = $("#crystalValueFour"); //crystalValueArr[3];
-
-$(document).ready(function() { //Checking if the page is ready to load the game.
-
-    function initializeGame() { //Initializing the game on page load.
-
-        // Generate random number from 19 - 120 for the computer number.
-        // start user value to 0.
-        // generate random values to each crystal.
-
-       chosenNumber.text("Match my number: " + [Math.floor(Math.random() * 120) + 19]); //chosen number will be anywhere between 19 - 120.
-
-        // yourNumber.text(0); //displaying user score on start of the game
-       
-       crystalValues();
-       winCounter();
-       lossCounter();
-    }
-
-    function crystalValues() { //generating crystal values
-        //Currently this will only output the same number amongst the 4 crystals
-        
-        // $("#crystalValue").on("click", function() {
-            crystalValueArr = []; //empty crystal value array to push 4 values into
-           
-            for (var i = 0; i < 4; i++) { //Loops 4 times
-
-                // generate random crystal number value
-                // if array is empty, store random value in a variable array
-                // create image array
-                // assign crystal value to a crystal image by index
-                // append crystal value and image to HTML DOM
-                
-                
-                var crystalValueStat = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
-
-                var intCrystalValue = parseInt(crystalValueStat);
-
-                crystalValueArr.push(intCrystalValue);
-
-                console.log("crystal one: " + crystalValueStat);
-            }
-
-            console.log(crystalValueArr);
-
-            $("#crystalValue").on("click", function() {
-                
-                $(".yourNumber").html(crystalValueArr[0]);
-                
-            }); 
-            
-            $("#crystalValueTwo").on("click", function() {
-                $(".yourNumber").html(crystalValueArr[1]);
-                
-            }); 
-
-            $("#crystalValueThree").on("click", function() {
-
-                $(".yourNumber").html(crystalValueArr[2]);
-                
-            }); 
-
-            $("#crystalValueFour").on("click", function() {
-
-                $(".yourNumber").html(crystalValueArr[3]);
-                
-            }); 
-
-    }
-        // $("#crystalValueTwo").on("click", function() {
-            
-        //     for (var i = 0; i < 4; i++) { 
-                
-        //         crystalValueArr = []; //empty crystal value array to push 4 values into
-        //         var crystalValueStat = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
-               
-        //         // if ((crystalValueArr === -1) || (crystalValueArr <= 3)) {
-
-        //         // crystalValue.attr(crystalValueStat);
-        //         // crystalValueArr.append(crystalImg);
-                
-        //         }
-
-        //         console.log("crystal two: " + crystalValueStat);
-        // });
-
-        // $("#crystalValueThree").on("click", function() {
-            
-        //     for (var i = 0; i < 4; i++) {
-                
-        //         crystalValueArr = []; //empty crystal value array to push 4 values into
-        //         var crystalValueStat = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
-               
-        //         // if ((crystalValueArr === -1) || (crystalValueArr <= 3)) {
-
-        //         // crystalValue.attr(crystalValueStat);
-        //         // crystalValueArr.append(crystalImg);
-                
-        //         }
-
-        //         console.log("crystal three: " + crystalValueStat);
-        // });
-
-        // $("#crystalValueFour").on("click", function() {
-            
-        //     for (var i = 0; i < 4; i++) { 
-                
-        //         crystalValueArr = []; //empty crystal value array to push 4 values into
-        //         var crystalValueStat = ([Math.floor(Math.random() * 13) + 1]); //generate random crystal value between 1 - 12
-               
-        //         // if ((crystalValueArr === -1) || (crystalValueArr <= 3)) {
-
-        //         // crystalValue.attr(crystalValueStat);
-        //         // crystalValueArr.append(crystalImg);
-                
-        //         }
-
-        //         console.log("crystal four: " + crystalValueStat);
-        // });
-    // } 
-
-        // $("#crystalValue").on("click", function() {
-
-        //     crystalValue.attr(crystalValueStat);
-            
-        //     // console.log("clicked the crystal");
-        //     // console.log(crystalValueStat);
-
-        // });
-
-        // $("#crystalValueTwo").on("click", function() {
-
-        //     crystalValueTwo.attr(crystalValueStat);
-
-        // });
-
-        // $("#crystalValueThree").on("click", function() {
-
-        //     crystalValueThree.attr(crystalValueStat);
-            
-        // });
-
-        // $("#crystalValueFour").on("click", function() {
-
-        //     crystalValueFour.attr(crystalValueStat);
-            
-        // });
-        
-    
-
-    function assignCrystalValue() { //Assigning the crystals different values every time game initiates
-
-    }
-    function userNumber() { //Being able to add randomly assigned crystal values to the user number
-
-    }
-
-    
-
-    function winCounter() { //Counts user wins if user is able to match their number to the computer number
-
-        if (yourNumber === chosenNumber) {
-
-            winCounter = 0;
-            winCounter++;
-            
-        }
-    }
-
-    function lossCounter() { //Counts user loss if user goes over the computer number.
-
-        if (yourNumber > chosenNumber) {
-
-            lossCounter = 0;
-            lossCounter++;
-    
-        }
-    }
-
-    initializeGame(); //Starts game when page loads.
-    
-});
